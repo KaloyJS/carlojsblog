947f4f45b19016689d170944d77d5676e7861115
Kaloy <kaloywebdev@gmail.com>
2019-10-21 21:45:51 -0400

    Complete core php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,3 +3,5 @@
 	require_once 'libraries/Core.php';
 	require_once 'libraries/Controller.php';
 	require_once 'libraries/Database.php';
+
+	
--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -16,10 +16,10 @@
 			$url = $this->getUrl();
 
 			// Look in controllers for first value
-			if(file_exists('../app/controllers/' . ucwords($url[0]) . 'php')){
+			if(file_exists('../app/controllers/' . ucwords($url[0]). '.php')){
 				// If exists, set as controller
 				$this->currentController = ucwords($url[0]);
-				// Unset 0 index
+				// Unset 0 Index
 				unset($url[0]);
 			}
 
@@ -29,7 +29,22 @@
 			// Instantiate controller class
 			$this->currentController = new $this->currentController;
 
+			// Check for second part of url
+			if(isset($url[1])){
+				// Check to see if method exists in controller
+				if(method_exists($this->currentController, $url[1])){
+					$this->currentMethod = $url[1];
 
+					// Unset 1 index
+					unset($url[1]);
+				}
+			}
+
+			// Get params
+			$this->params = $url ? array_values($url) : [];
+
+			// Call a callback with array of params
+			call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
 
 		}
 
